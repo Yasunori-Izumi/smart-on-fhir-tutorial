1e6cfde685e8cbd68d3dee84f600761709b68210
izumi yasunori <y.izumi@allm.inc>
2021-04-16 15:58:53 +0900

    change js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,10 +16,11 @@
            type: 'Observation',
            query: {
              code: {
-               $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                     'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                     'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-             }
+              // $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+              // 'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+              // 'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+              $or: ['http://loinc.org|58941-6']
+}
            }
          });
 
@@ -41,8 +42,8 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           console.log("byCodes:");
-          console.log(byCodes('8480-6'));
-          console.log(byCodes('8462-4'));
+          console.log(byCodes('58941-6'));
+          console.log(byCodes('58941-6'));
 
           var gender = patient.gender;
 
@@ -57,11 +58,11 @@
           // Observations
           // lymph = byCodes('26478-8');
           // Cerner SoF Tutorial Observations
-           var height = byCodes('8302-2');
-           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-           var hdl = byCodes('2085-9');
-           var ldl = byCodes('2089-1');
+           var height = byCodes('58941-6');
+           var systolicbp = getBloodPressureValue(byCodes('58941-6'),'58941-6');
+           var diastolicbp = getBloodPressureValue(byCodes('58941-6'),'58941-6');
+           var hdl = byCodes('58941-6');
+           var ldl = byCodes('58941-6');
 
 
           var p = defaultPatient();
